6c2df7714761bbe7c4d6bc4613b2bf30c850e0a7
Shreyansh mIshra <2130153090@ipec.org.in>
2023-10-12 17:25:08 +0530

    adding all files

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
+        btn1.setOnClickListener(View.OnClickListener));
+
+
+    }
+}
